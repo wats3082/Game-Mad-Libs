e5f42ce918f7501a6e13d28c3b7bda60f9bcbd87
wats3082 <92612217+wats3082@users.noreply.github.com>
2021-12-19 11:34:18 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Mad Libs is where you input random words into a story
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter your fav celebrity: ")
+
+
+
+
+print("Roses are " + color)
+print(plural_noun + " are blue, ")
+print("I love " + celebrity)
