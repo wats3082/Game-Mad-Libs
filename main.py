847865b5d002d4c6a12b033522f330b2ae163b63
Russell Watson <92612217+wats3082@users.noreply.github.com>
2024-11-15 21:16:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,14 @@ print("Pondering the universe.....")
 time.sleep 
 print(".....")
 
-time.sleep 5
+time.sleep 2
 
 print(color + "fade's to dusk,")
+time.sleep 2
+
 print("Our " + plural_noun + " strikes without pause- ")
+time.sleep 2
+
 print("Death's shadow draws near.")
 #Crimson paints the sky,
 #A scythe cuts the air in haste—
@@ -22,7 +26,14 @@ print("Death's shadow draws near.")
 #Golden light turns cold,
 #A sword gleams with finality—
 #Death waits, ever still.
-time.sleep 5
+time.sleep 2
+print("Pondering the universe.....")
+time.sleep 2
+
 print(color + "spills like ink,")
+time.sleep 2
+
 print("A " + plural_noun + " strikes, sharp with regret- ")
+time.sleep 2
+
 print("The void calls softly.")
