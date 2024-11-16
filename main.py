cf0572eb26b822808d93a13e207822cb207d3161
Russell Watson <92612217+wats3082@users.noreply.github.com>
2024-11-15 21:15:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 #Mad Libs is where you input random words into a story
+print("Welcome to virion haiku.")
+
+
 
 color = input("Enter a color: ")
-plural_noun = input("Enter a plural noun: ")
-celebrity = input("Enter your fav celebrity: ")
+plural_noun = input("Enter a weapon: ")
+celebrity = input("Guess your age of death: ")
 
+print("Pondering the universe.....")
+time.sleep 
+print(".....")
 
+time.sleep 5
 
-
-print("Roses are " + color)
-print(plural_noun + " are blue, ")
-print("I love " + celebrity)
+print(color + "fade's to dusk,")
+print("Our " + plural_noun + " strikes without pause- ")
+print("Death's shadow draws near.")
+#Crimson paints the sky,
+#A scythe cuts the air in haste—
+#Shadows close around.
+#Golden light turns cold,
+#A sword gleams with finality—
+#Death waits, ever still.
+time.sleep 5
+print(color + "spills like ink,")
+print("A " + plural_noun + " strikes, sharp with regret- ")
+print("The void calls softly.")
