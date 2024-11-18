7a64e5db7a89d618bb35f046ed965ee65c888483
Russell Watson <92612217+wats3082@users.noreply.github.com>
2024-11-17 18:07:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,59 @@
 #Mad Libs is where you input random words into a story
+import time
+
 print("Welcome to virion haiku.")
 
 
+time.sleep(2)
 
 color = input("Enter a color: ")
 plural_noun = input("Enter a weapon: ")
 celebrity = input("Guess your age of death: ")
+time.sleep(2)
+time.sleep(2)
 
 print("Pondering the universe.....")
-time.sleep 
-print(".....")
+time.sleep(2)
 
-time.sleep 2
+print("............................")
+time.sleep(2)
 
-print(color + "fade's to dusk,")
-time.sleep 2
+
+print(color + " fade's to dusk,")
+time.sleep(2)
 
 print("Our " + plural_noun + " strikes without pause- ")
-time.sleep 2
+time.sleep(2)
 
 print("Death's shadow draws near.")
+
+print("............................")
+time.sleep(2)
+print("............................")
+time.sleep(2)
+
 #Crimson paints the sky,
 #A scythe cuts the air in haste—
 #Shadows close around.
 #Golden light turns cold,
 #A sword gleams with finality—
 #Death waits, ever still.
-time.sleep 2
-print("Pondering the universe.....")
-time.sleep 2
 
-print(color + "spills like ink,")
-time.sleep 2
+
+time.sleep(2)
+
+print("Pondering the universe.....")
+time.sleep(2)
+
+print("............................")
+time.sleep(2)
+print(color + " spills like ink,")
+time.sleep(2)
+
 
 print("A " + plural_noun + " strikes, sharp with regret- ")
-time.sleep 2
+time.sleep(2)
 
 print("The void calls softly.")
+
+time.sleep(2)
